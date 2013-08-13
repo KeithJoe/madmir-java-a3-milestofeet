2851b4cb9e95ea6bd484f25b1e99b54a5f811b64
KeithJoe <keithjoe@192.168.0.38>
2013-08-13 11:56:31 -0400

    This is a my revised assignment, I found a mistake in the first on.

--- src/MilesToFeetInteractive.java
+++ src/MilesToFeetInteractive.java
@@ -8,14 +8,16 @@ public class MilesToFeetInteractive {
 		//Declare an initialize name constant FEET_PER_MILE
 		final int FEET_PER_MILE = 5280;
 		//Declare and initialize variables
-		float uncleMiles = 85f;
-		float uncleFeet = uncleMiles * 	FEET_PER_MILE;
+		float uncleMiles = 85f; 
+		float uncleFeet;
+		uncleFeet = uncleMiles * FEET_PER_MILE;
 		Scanner sc = new Scanner(System.in);
 				
 		//Display output
 		System.out.print("Enter a number: ");
 		uncleMiles = sc.nextFloat();
-		System.out.println("The distance to my uncles house is " + uncleMiles + "miles");
+		uncleFeet = uncleMiles * FEET_PER_MILE;
+		System.out.println("The distance to my uncles house is " + uncleMiles + " miles");
 		System.out.println("That's " + uncleFeet + "feet");
 		JOptionPane.showMessageDialog(null, "How many miles to your uncles house? " + uncleMiles + " miles. Thats " + uncleFeet + "feet!");
 		
